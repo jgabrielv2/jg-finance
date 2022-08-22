eb251c50affcd918295b3584e8fdb4615c2ca4f8
João Gabriel <jgabriel.vidal@outlook.com>
2022-08-22 02:21:36 -0300

    projeto inicial

---
+++ src/main/java/br/com/jgfinance/JgFinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.jgfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jgfinance/JgFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jgfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
